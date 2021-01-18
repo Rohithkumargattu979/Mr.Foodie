54ca7f65281710f7ba4f8ca9dc776293792d8fcc
Rohith Kumar Gattu <57759452+Rohithkumargattu979@users.noreply.github.com>
2021-01-18 11:33:50 +0530

    finish added

--- app/src/main/java/com/example/mrfoodie/Login.java
+++ app/src/main/java/com/example/mrfoodie/Login.java
@@ -83,6 +83,7 @@ public class Login extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                            finish();
                         }else {
                             Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
